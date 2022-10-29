5160fba0ec27ca2b44c38d2ba3e4ca8d6d923784
TagoAB <116982329+TagoAB@users.noreply.github.com>
2022-10-30 00:54:43 +0200

    First project

---
+++ tago/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Hello Test GitHub</h2>
+          TaGo
+      </header>
+    </div>
+  );
+}
+
+export default App;
